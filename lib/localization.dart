66e86f6a82531475783353dd2905dc212b3b43e2
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-02-06 10:01:36 +0530

    print

--- lib/localization.dart
+++ lib/localization.dart
@@ -10,67 +10,68 @@ class LocalizationService {
   LocalizationService(this.locale);
 
   static LocalizationService? of(BuildContext context) {
+    print("LocalizationService of $context");
     return Localizations.of<LocalizationService>(context, LocalizationService);
   }
 
   Map<String, String> _localizedStrings = {};
 
   Future<void> load() async {
-    final jsonString =
-        await rootBundle.loadString('lang/${locale.languageCode}.json');
-    _localizedStrings = json.decode(jsonString) as Map<String, String>;
-    /* jsonMap.forEach((key, value) {
-      _localizedStrings[key]=value.toString();
-    });*/
+    print("LocalizationService load lang/${locale.languageCode}.json");
+    final jsonString = await rootBundle.loadString('lang/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString) as Map<String, String>;
+    jsonMap.forEach((key, value) {
+      _localizedStrings[key] = value.toString();
+    });
   }
 
   String? translate(String key) {
+    print("LocalizationService translate $key");
     return _localizedStrings[key];
   }
 
   static const supportedLocales = [
     Locale("en", "US"),
     Locale("hi", "IN"),
-    Locale("odi", "IN"),
+    Locale("odi", "GB"),
   ];
 
-  static Locale? localeResolutionCallback(
-      Locale? locale, Iterable<Locale>? supportedLocales) {
-   return supportedLocales
-        !.firstWhere((element) => element.languageCode == locale!.languageCode
-   ,orElse: ()=>supportedLocales.first);
+  static Locale? localeResolutionCallback(Locale? locale, Iterable<Locale>? supportedLocales) {
+    print(
+        "LocalizationService localeResolutionCallback ${locale?.languageCode} ${supportedLocales?.map((e) => e.languageCode).toList()}");
+    return supportedLocales!
+        .firstWhere((element) => element.languageCode == locale!.languageCode, orElse: () => supportedLocales.first);
   }
 
- static   LocalizationsDelegate<LocalizationService> _delegate=_LocalizatioServiceDelegates();
+  static const LocalizationsDelegate<LocalizationService> _delegate = _LocalizatioServiceDelegates();
 
-  static const localizationDelegate=[
+  static const localizationDelegate = [
     GlobalMaterialLocalizations.delegate, // works on alert, ok, cancel translation
-    GlobalWidgetsLocalizations.delegate,// RTL and LTR
+    GlobalWidgetsLocalizations.delegate, // RTL and LTR
     _delegate
-
-
   ];
 }
 
-
-class _LocalizatioServiceDelegates extends LocalizationsDelegate<LocalizationService>{
+class _LocalizatioServiceDelegates extends LocalizationsDelegate<LocalizationService> {
+  const _LocalizatioServiceDelegates();
   @override
   bool isSupported(Locale locale) {
+    print("_LocalizatioServiceDelegates isSupported ${locale.languageCode}");
     return LocalizationService.supportedLocales.contains(locale);
   }
 
   @override
-  Future<LocalizationService> load(Locale locale) async{
-    LocalizationService service=LocalizationService(locale);
+  Future<LocalizationService> load(Locale locale) async {
+    print("_LocalizatioServiceDelegates load ${locale.languageCode}");
+    LocalizationService service = LocalizationService(locale);
     await service.load();
     return service;
   }
 
   @override
   bool shouldReload(covariant LocalizationsDelegate<LocalizationService> old) {
-
-   // return old!=this;
+    print("_LocalizatioServiceDelegates shouldReload ${old.hashCode} ${this.hashCode} ${old != this}");
+    // return old!=this;
     return false;
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/localization.dart';
 import 'package:flutter_localization/second_page.dart';
 
 void main() {
@@ -11,20 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print("MyApp  $context");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: LocalizationService.of(context)?.translate("demoHomePage") ?? "--"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -37,29 +38,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    print("MyHomePage  $context");
 
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
